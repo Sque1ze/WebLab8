cd9b514b8d1852bd930ba42d62e3a59c67478f4c
Sque1ze <102423822+Sque1ze@users.noreply.github.com>
2025-04-07 13:21:47 +0300

    Create index.vue

---
+++ pages/posts/components/table/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="w-full">
+        <div class="flex justify-center">
+            <div class="w-full">
+                <div class="card">
+                    <div class="card-body w-full">
+                        <table class="table table-auto">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Автор</th>
+                                    <th>Категорія</th>
+                                    <th>Заголовок</th>
+                                    <th>Дата публікації</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="post in posts">
+                                    <td>{{ post.id }}</td>
+                                    <td>{{ post.user.name }}</td>
+                                    <td>{{ post.category.title }}</td>
+                                    <td><NuxtLink :to="`/post?id=${post.id}`">{{ post.title }}</NuxtLink></td>
+                                    <td>{{ post.published_at }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { IPost } from '~/types';
+
+defineProps<{
+  posts: [IPost]
+}>();
+</script>
